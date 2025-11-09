af2463e06fe1c05beef77dd0158a74cee0f76586
KyleSebion <kyle@kylesebion.com>
2025-11-09 03:56:37 -0600

    Refactor main function to implement LED toggling and logging in separate threads

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,24 @@
-use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::delay::Delay;
+use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::PinDriver;
+use esp_idf_hal::peripherals::Peripherals;
 
 fn main() {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
     let peripherals = Peripherals::take().unwrap();
-    let delay = Delay::default();
-    let mut led = PinDriver::output(peripherals.pins.gpio8).unwrap();
-    loop {
-        log::info!("Hello, world!");
-        led.toggle().unwrap();
-        delay.delay_ms(100);
-    }
-}
-// use esp_idf_hal::{delay::FreeRtos, gpio::PinDriver, ;
-
-// fn main() {
     
-//     // Initialize Pin 8 as an output to drive the LED
-//     let mut led_pin = PinDriver::output(peripherals.pins.gpio8).unwrap();
-
-//     // Loop forever blinking the LED on/off every 500ms
-//     loop {
-//         // Inverse logic to turn LED on
-//         led_pin.set_low().unwrap();
-//         println!("LED ON");
-//         FreeRtos::delay_ms(1000);
-
-//         led_pin.set_high().unwrap();
-//         println!("LED OFF");
-//         FreeRtos::delay_ms(1000);
-//     }
-// }
+    let mut led = PinDriver::output(peripherals.pins.gpio8).unwrap();
+    let _ = std::thread::Builder::new().stack_size(2000).spawn(|| {
+        loop {
+            log::info!("Nothing");
+            FreeRtos::delay_ms(500);
+        }
+    });
+    let _ = std::thread::Builder::new().stack_size(2000).spawn(move || {
+        loop {
+            log::info!("LED Toggle");
+            led.toggle().unwrap();
+            FreeRtos::delay_ms(100);
+        }
+    });
+}
