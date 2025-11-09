ae75de96b1932de0a71748439838f3602e7bb1e5
KyleSebion <kyle@kylesebion.com>
2025-11-09 03:57:04 -0600

    Refactor thread spawning for improved readability in main function

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,17 @@ fn main() {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
     let peripherals = Peripherals::take().unwrap();
-    
+
     let mut led = PinDriver::output(peripherals.pins.gpio8).unwrap();
-    let _ = std::thread::Builder::new().stack_size(2000).spawn(|| {
-        loop {
-            log::info!("Nothing");
-            FreeRtos::delay_ms(500);
-        }
+    let _ = std::thread::Builder::new().stack_size(2000).spawn(|| loop {
+        log::info!("Nothing");
+        FreeRtos::delay_ms(500);
     });
-    let _ = std::thread::Builder::new().stack_size(2000).spawn(move || {
-        loop {
+    let _ = std::thread::Builder::new()
+        .stack_size(2000)
+        .spawn(move || loop {
             log::info!("LED Toggle");
             led.toggle().unwrap();
             FreeRtos::delay_ms(100);
-        }
-    });
+        });
 }
