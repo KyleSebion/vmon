0dcdaaf7c567df65ab2b147856852ee161282562
KyleSebion <kyle@kylesebion.com>
2025-11-12 01:16:55 -0600

    beginning wifi testing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,55 @@
+use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::peripherals::Peripherals;
-use std::thread::sleep;
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::wifi::AccessPointConfiguration;
+use esp_idf_svc::wifi::AuthMethod;
+use esp_idf_svc::wifi::Configuration;
+use esp_idf_svc::wifi::EspWifi;
+use esp_idf_svc::wifi::Protocol;
+use esp_idf_svc::wifi::WifiEvent;
 use std::thread::Builder as Thread;
-use std::time::Duration;
+// cargo remove esp-idf-sys if not needed
 
 fn main() {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
+    log::set_max_level(log::LevelFilter::Debug);
+
     let peripherals = Peripherals::take().unwrap();
     let gpio8 = peripherals.pins.gpio8;
 
+    let sys_loop = EspSystemEventLoop::take().unwrap();
+    sys_loop
+        .subscribe::<WifiEvent, _>(|ev| log::info!("WifiEvent: {ev:?}"))
+        .unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
+    let mut wifi = EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs)).unwrap();
+    let conf = Configuration::AccessPoint(AccessPointConfiguration {
+        ssid: "ESP2".try_into().unwrap(),
+        ssid_hidden: false,
+        channel: 1,
+        secondary_channel: None,
+        protocols: Protocol::P802D11BGN.into(),
+        auth_method: AuthMethod::WPA2Personal,
+        password: "kspass1234".try_into().unwrap(),
+        max_connections: 10,
+    });
+    wifi.set_configuration(&conf).unwrap();
+    wifi.start().unwrap();
+    FreeRtos::delay_ms(1000);
+
     let t1 = Thread::new().stack_size(2000).spawn(|| loop {
         log::info!("Nothing");
-        sleep(Duration::from_millis(500));
+        FreeRtos::delay_ms(2000);
     });
     let t2 = Thread::new().stack_size(2000).spawn(move || {
         let mut led = PinDriver::output(gpio8).unwrap();
         loop {
             log::info!("LED Toggle");
             led.toggle().unwrap();
-            sleep(Duration::from_millis(100));
+            FreeRtos::delay_ms(1000); // switch to use std::thread::sleep;use std::time::Duration;sleep(Duration::from_millis(100)); at end
         }
     });
     for t in [t1, t2] {
