c0401b89ae9e2440f00748633710c0d7464b918a
KyleSebion <kyle@kylesebion.com>
2025-12-03 00:16:07 -0600

    Refactor file handling and measurement recording; integrate I2C devices for RTC and INA219

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
+#![allow(dead_code)]
 use anyhow::Ok;
 use anyhow::Result;
 use esp_idf_hal::delay::TickType;
 use esp_idf_hal::delay::TickType_t;
 use esp_idf_hal::units::FromValueType;
 use esp_idf_svc::eventloop::EspSystemEventLoop;
-use esp_idf_svc::hal::adc::attenuation::DB_11 as Atten11dB;
-use esp_idf_svc::hal::adc::oneshot::config::AdcChannelConfig;
-use esp_idf_svc::hal::adc::oneshot::config::Calibration::Curve as CalCurve;
-use esp_idf_svc::hal::adc::oneshot::AdcChannelDriver;
-use esp_idf_svc::hal::adc::oneshot::AdcDriver;
-use esp_idf_svc::hal::adc::Resolution::Resolution12Bit;
 use esp_idf_svc::hal::delay::FreeRtos;
 use esp_idf_svc::hal::gpio::PinDriver;
 use esp_idf_svc::hal::i2c::I2cConfig;
@@ -23,7 +18,6 @@ use esp_idf_svc::sys::esp_vfs_fat_info;
 use esp_idf_svc::sys::esp_vfs_fat_mount_config_t;
 use esp_idf_svc::sys::esp_vfs_fat_spiflash_mount_rw_wl;
 use esp_idf_svc::sys::wl_handle_t;
-use esp_idf_svc::sys::EspError;
 use esp_idf_svc::wifi::AccessPointConfiguration;
 use esp_idf_svc::wifi::AuthMethod;
 use esp_idf_svc::wifi::Configuration as WiFiConf;
@@ -39,6 +33,7 @@ use std::sync::LazyLock;
 use std::sync::Mutex;
 use std::sync::MutexGuard;
 use std::thread;
+use std::thread::Builder as Thread;
 
 const STOR_PATH: &CStr = c"/storage";
 fn mount_storage() -> Result<()> {
@@ -74,49 +69,74 @@ fn get_storage_free_space() -> Result<u64> {
     Ok(free)
 }
 
-const DATA_FILE_PATH: &str = "/storage/data.csv";
-static DATA_FILE: LazyLock<Mutex<File>> = LazyLock::new(|| {
+fn open_data_file() -> Mutex<File> {
+    const DATA_FILE_PATH: &str = "/storage/data.csv";
     Mutex::new(
         OpenOptions::new()
-            .create(true)
-            .append(true)
             .read(true)
+            .append(true)
+            .create(true)
             .open(DATA_FILE_PATH)
             .unwrap_or_else(|_| panic!("failed to open file: {DATA_FILE_PATH}")),
     )
-});
-fn get_lock<'a>() -> Result<MutexGuard<'a, File>> {
-    DATA_FILE
-        .lock()
-        .map_err(|e| anyhow::anyhow!("get_lock error: {:?}", e))
 }
-fn append_data(d: &str) -> Result<()> {
-    const STOR_MIN_FREE: u64 = 512 * 1024;
-    if get_storage_free_space()? < STOR_MIN_FREE {
-        log::warn!("append_data canceled due to lack of minimum free space");
-        return Ok(());
+fn open_settings_file() -> Mutex<File> {
+    const SETTINGS_FILE_PATH: &str = "/storage/setting.json";
+    Mutex::new(
+        OpenOptions::new()
+            .read(true)
+            .write(true)
+            .truncate(false)
+            .create(true)
+            .open(SETTINGS_FILE_PATH)
+            .unwrap_or_else(|_| panic!("failed to open file: {SETTINGS_FILE_PATH}")),
+    )
+}
+struct LockedFile {
+    locker: LazyLock<Mutex<File>>,
+}
+impl LockedFile {
+    //split to data and settings
+    pub const fn new(init: fn() -> Mutex<File>) -> LockedFile {
+        LockedFile {
+            locker: LazyLock::new(init),
+        }
     }
-    let mut f = get_lock()?;
-    if f.metadata()?.len() == 0 {
-        writeln!(f, "uptime_ms,rtc_ts,mv")?;
+    fn lock(&self) -> Result<MutexGuard<'_, File>> {
+        self.locker
+            .lock()
+            .map_err(|e| anyhow::anyhow!("lock error: {e}"))
     }
-    writeln!(f, "{d}")?;
-    f.sync_all()?;
-    Ok(())
+    fn append_data(&self, d: &str) -> Result<()> {
+        const STOR_MIN_FREE: u64 = 512 * 1024;
+        if get_storage_free_space()? < STOR_MIN_FREE {
+            log::warn!("append_data canceled due to lack of minimum free space");
+            return Ok(());
+        }
+        let mut f = self.lock()?;
+        if f.metadata()?.len() == 0 {
+            writeln!(f, "uptime_ms,rtc_ts,w,v,a")?;
+        }
+        writeln!(f, "{d}")?;
+        f.sync_all()?;
+        Ok(())
+    }
+    fn clear_data(&self) -> Result<()> {
+        let f = self.lock()?;
+        f.set_len(0)?;
+        f.sync_all()?;
+        Ok(())
+    }
+    fn read_data(&self) -> Result<String> {
+        let mut f = self.lock()?;
+        f.rewind()?;
+        let mut s = String::new();
+        f.read_to_string(&mut s)?;
+        Ok(s)
+    }
 }
-fn clear_data() -> Result<()> {
-    let f = get_lock()?;
-    f.set_len(0)?;
-    f.sync_all()?;
-    Ok(())
-}
-fn read_data() -> Result<String> {
-    let mut f = get_lock()?;
-    f.rewind()?;
-    let mut s = String::new();
-    f.read_to_string(&mut s)?;
-    Ok(s)
-}
+static DATA_FILE: LockedFile = LockedFile::new(open_data_file);
+static SETTINGS_FILE: LockedFile = LockedFile::new(open_settings_file);
 
 fn reset_then_sleep(usec: u64) -> ! {
     unsafe { esp_deep_sleep(usec) }
@@ -131,139 +151,261 @@ fn uptime_usec() -> i64 {
     unsafe { esp_timer_get_time() }
 }
 
-type EspResultU16 = std::result::Result<u16, EspError>;
-type EspResultU32 = std::result::Result<u32, EspError>;
-trait AdcReadFn: FnMut() -> EspResultU16 {}
-impl<T: FnMut() -> EspResultU16> AdcReadFn for T {}
-fn get_oversampled_mv<F: AdcReadFn>(adc_read_fn: &mut F) -> Result<u32> {
-    const SAMPLE_SZ: usize = 16;
-    let s = (0..SAMPLE_SZ)
-        .map(|_| adc_read_fn().map(|v| v as u32))
-        .sum::<EspResultU32>()?;
-    Ok(s / SAMPLE_SZ as u32)
-}
-fn get_smoothed_mv(mv: u32) -> u32 {
-    const SMOOTH_BUF_SZ: usize = 8;
+fn get_smoothed<const I: usize>(val: f64) -> f64 {
+    const SMOOTH_ARRAY_COUNT: usize = 3;
+    const SMOOTH_COUNT: usize = 8;
     #[link_section = ".rtc.data"]
-    static mut SMOOTH_BUF_I: usize = usize::MAX;
+    static mut SMOOTH_BUF_IS: [usize; SMOOTH_ARRAY_COUNT] = [usize::MAX; SMOOTH_ARRAY_COUNT];
     #[link_section = ".rtc.data"]
-    static mut SMOOTH_BUF: [u32; SMOOTH_BUF_SZ] = [0; SMOOTH_BUF_SZ];
+    static mut SMOOTH_BUFS: [[f64; SMOOTH_COUNT]; SMOOTH_ARRAY_COUNT] =
+        [[0_f64; SMOOTH_COUNT]; SMOOTH_ARRAY_COUNT];
     unsafe {
-        if SMOOTH_BUF_I == usize::MAX {
-            #[expect(static_mut_refs)]
-            SMOOTH_BUF.fill(mv);
-            SMOOTH_BUF_I = 0;
+        let smooth_buf = &mut SMOOTH_BUFS[I];
+        let smooth_buf_i = &mut SMOOTH_BUF_IS[I];
+        if *smooth_buf_i == usize::MAX {
+            smooth_buf.fill(val);
+            *smooth_buf_i = 0;
         } else {
-            SMOOTH_BUF[SMOOTH_BUF_I] = mv;
-            SMOOTH_BUF_I = (SMOOTH_BUF_I + 1) % SMOOTH_BUF_SZ;
+            smooth_buf[*smooth_buf_i] = val;
+            *smooth_buf_i = (*smooth_buf_i + 1) % SMOOTH_COUNT;
         }
-        #[expect(static_mut_refs)]
-        let s = SMOOTH_BUF.iter().sum::<u32>();
-        s / SMOOTH_BUF_SZ as u32
+        let s = smooth_buf.iter().sum::<f64>();
+        s / SMOOTH_COUNT as f64
     }
 }
-fn convert_adc_to_mv(mv: u32) -> u32 {
-    const R1: u32 = 33000;
-    const R2: u32 = 4700;
-    let mv = mv * (R1 + R2);
-    mv / R2
-}
 
-const DS3231_ADDR: u8 = 0x68;
-const I2C_TIMEOUT: TickType_t = TickType::new_millis(1).0;
-fn dec_to_bcd(d: u8) -> u8 {
-    ((d / 10) << 4) | (d % 10)
-}
-pub fn set_rtc(
-    i2c: &mut I2cDriver,
+struct RtcDateTime {
     year: u16,
     month: u8,
     day: u8,
     hour: u8,
     minute: u8,
     second: u8,
-) -> Result<()> {
-    let year = (year - 2000) as u8;
-    let data = [
-        0x00,
-        dec_to_bcd(second),
-        dec_to_bcd(minute),
-        dec_to_bcd(hour),
-        dec_to_bcd(0), // Day of week (not used here)
-        dec_to_bcd(day),
-        dec_to_bcd(month),
-        dec_to_bcd(year),
-    ];
-    i2c.write(DS3231_ADDR, &data, I2C_TIMEOUT)?;
-    Ok(())
 }
-fn bcd_to_dec(b: u8) -> u8 {
-    (b >> 4) * 10 + (b & 0x0F)
+impl RtcDateTime {
+    fn new(year: u16, month: u8, day: u8, hour: u8, minute: u8, second: u8) -> Self {
+        Self {
+            year,
+            month,
+            day,
+            hour,
+            minute,
+            second,
+        }
+    }
 }
-pub fn read_rtc(i2c: &mut I2cDriver) -> Result<(u16, u8, u8, u8, u8, u8)> {
-    i2c.write(DS3231_ADDR, &[0x00], I2C_TIMEOUT)?;
-    let mut buf = [0u8; 7];
-    i2c.read(DS3231_ADDR, &mut buf, I2C_TIMEOUT)?;
-    let second = bcd_to_dec(buf[0] & 0x7F);
-    let minute = bcd_to_dec(buf[1]);
-    let hour = bcd_to_dec(buf[2] & 0x3F);
-    let day = bcd_to_dec(buf[4]);
-    let month = bcd_to_dec(buf[5] & 0x1F);
-    let year = 2000 + bcd_to_dec(buf[6]) as u16;
-    Ok((year, month, day, hour, minute, second))
+struct DS3231 {
+    addr: u8,
+    timeout: TickType_t,
 }
-fn read_rtc_str(i2c: &mut I2cDriver) -> Result<String> {
-    let (year, month, day, hour, minute, second) = read_rtc(i2c)?;
-    Ok(format!(
-        "{year:04}-{month:02}-{day:02} {hour:02}:{minute:02}:{second:02}"
-    ))
+impl DS3231 {
+    fn dec_to_bcd(d: u8) -> u8 {
+        ((d / 10) << 4) | (d % 10)
+    }
+    fn bcd_to_dec(b: u8) -> u8 {
+        (b >> 4) * 10 + (b & 0x0F)
+    }
+    fn new(addr: u8) -> Self {
+        Self {
+            addr,
+            timeout: TickType::new_millis(100).0,
+        }
+    }
+    fn set_rtc(&mut self, i2c: &mut I2cDriver, dt: &RtcDateTime) -> Result<()> {
+        let year = (dt.year - 2000) as u8;
+        let data = [
+            0x00,
+            Self::dec_to_bcd(dt.second),
+            Self::dec_to_bcd(dt.minute),
+            Self::dec_to_bcd(dt.hour),
+            Self::dec_to_bcd(0), // Day of week (not used)
+            Self::dec_to_bcd(dt.day),
+            Self::dec_to_bcd(dt.month),
+            Self::dec_to_bcd(year),
+        ];
+        i2c.write(self.addr, &data, self.timeout)?;
+        Ok(())
+    }
+    fn read_rtc(&mut self, i2c: &mut I2cDriver) -> Result<RtcDateTime> {
+        i2c.write(self.addr, &[0x00], self.timeout)?;
+        let mut buf = [0u8; 7];
+        i2c.read(self.addr, &mut buf, self.timeout)?;
+        let second = Self::bcd_to_dec(buf[0] & 0x7F);
+        let minute = Self::bcd_to_dec(buf[1]);
+        let hour = Self::bcd_to_dec(buf[2] & 0x3F);
+        let day = Self::bcd_to_dec(buf[4]);
+        let month = Self::bcd_to_dec(buf[5] & 0x1F);
+        let year = 2000 + Self::bcd_to_dec(buf[6]) as u16;
+        Ok(RtcDateTime::new(year, month, day, hour, minute, second))
+    }
+    fn read_rtc_str(&mut self, i2c: &mut I2cDriver) -> Result<String> {
+        let RtcDateTime {
+            year,
+            month,
+            day,
+            hour,
+            minute,
+            second,
+        } = self.read_rtc(i2c)?;
+        Ok(format!(
+            "{year:04}-{month:02}-{day:02} {hour:02}:{minute:02}:{second:02}"
+        ))
+    }
 }
-fn record_voltage<F: AdcReadFn>(adc_read_fn: &mut F, i2c: &mut I2cDriver) -> Result<()> {
+struct Ina219 {
+    addr: u8,
+    timeout: TickType_t,
+    r_shunt: f64,              // Ω
+    max_expected_current: f64, // A
+    current_lsb: f64,
+    power_lsb: f64,
+    calibration: u16,
+    conf: u16,
+}
+impl Ina219 {
+    const REG_CONF: u8 = 0x00;
+    const REG_SHUNT_V: u8 = 0x01;
+    const REG_BUS_V: u8 = 0x02;
+    const REG_POWER_W: u8 = 0x03;
+    const REG_CURRENT_A: u8 = 0x04;
+    const REG_CALIBRATE: u8 = 0x05;
+    const INTERNAL_FIXED_VALUE: f64 = 0.04096;
+    const SHUNT_VOLTAGE_LSB: f64 = 0.000010; // 10 μV
+    const BUS_VOLTAGE_LSB: f64 = 0.004; // 4 mV
+    fn new(addr: u8, r_shunt: f64, max_expected_current: f64, conf: u16) -> Self {
+        let current_lsb = max_expected_current / 2_f64.powi(15);
+        Self {
+            addr,
+            timeout: TickType::new_millis(100).0,
+            r_shunt,
+            max_expected_current,
+            current_lsb,
+            power_lsb: 20_f64 * current_lsb,
+            calibration: (Self::INTERNAL_FIXED_VALUE / (current_lsb * r_shunt)) as u16,
+            conf,
+        }
+    }
+    fn read_u16(&mut self, i2c: &mut I2cDriver, reg: u8) -> Result<u16> {
+        i2c.write(self.addr, &[reg], self.timeout)?;
+        let mut buf = [0u8; 2];
+        i2c.read(self.addr, &mut buf, self.timeout)?;
+        Ok(u16::from_be_bytes(buf))
+    }
+    fn read_i16(&mut self, i2c: &mut I2cDriver, reg: u8) -> Result<i16> {
+        self.read_u16(i2c, reg).map(|v| v as i16)
+    }
+    fn write_u16(&mut self, i2c: &mut I2cDriver, reg: u8, v: u16) -> Result<()> {
+        let mut buf = [reg, 0, 0];
+        buf[1..].copy_from_slice(&v.to_be_bytes());
+        i2c.write(self.addr, &buf, self.timeout)?;
+        Ok(())
+    }
+    fn write_conf(&mut self, i2c: &mut I2cDriver) -> Result<()> {
+        self.write_u16(i2c, Self::REG_CONF, self.conf)
+    }
+    fn write_calibration(&mut self, i2c: &mut I2cDriver) -> Result<()> {
+        self.write_u16(i2c, Self::REG_CALIBRATE, self.calibration)
+    }
+    fn read_shunt_v(&mut self, i2c: &mut I2cDriver) -> Result<f64> {
+        self.read_i16(i2c, Self::REG_SHUNT_V)
+            .map(|v| v as f64 * Self::SHUNT_VOLTAGE_LSB)
+    }
+    fn read_bus_v(&mut self, i2c: &mut I2cDriver) -> Result<f64> {
+        self.read_u16(i2c, Self::REG_BUS_V)
+            .map(|v| (v >> 3) as f64 * Ina219::BUS_VOLTAGE_LSB)
+    }
+    fn read_w(&mut self, i2c: &mut I2cDriver) -> Result<f64> {
+        self.read_u16(i2c, Self::REG_POWER_W)
+            .map(|v| v as f64 * self.power_lsb)
+    }
+    fn read_a(&mut self, i2c: &mut I2cDriver) -> Result<f64> {
+        self.read_i16(i2c, Self::REG_CURRENT_A)
+            .map(|v| v as f64 * self.current_lsb)
+    }
+    fn read_v(&mut self, i2c: &mut I2cDriver) -> Result<f64> {
+        let sv = self.read_shunt_v(i2c)?;
+        let bv = self.read_bus_v(i2c)?;
+        Ok(if sv.is_sign_negative() { bv } else { sv + bv })
+    }
+}
+struct I2cDevices<'a> {
+    i2c: I2cDriver<'a>,
+    ds3231: DS3231,
+    ina219: Ina219,
+}
+impl<'a> I2cDevices<'a> {
+    fn new(i2c: I2cDriver<'a>, ds3231: DS3231, ina219: Ina219) -> Result<Self> {
+        let mut s = Self {
+            i2c,
+            ds3231,
+            ina219,
+        };
+        let i2c = &mut s.i2c;
+        s.ina219.write_conf(i2c)?;
+        s.ina219.write_calibration(i2c)?;
+        Ok(s)
+    }
+    fn set_ds3231_rtc(&mut self, dt: &RtcDateTime) -> Result<()> {
+        self.ds3231.set_rtc(&mut self.i2c, dt)
+    }
+    fn read_ds3231_rtc_str(&mut self) -> Result<String> {
+        self.ds3231.read_rtc_str(&mut self.i2c)
+    }
+    fn read_ina219_w(&mut self) -> Result<f64> {
+        self.ina219.read_w(&mut self.i2c)
+    }
+    fn read_ina219_v(&mut self) -> Result<f64> {
+        self.ina219.read_v(&mut self.i2c)
+    }
+    fn read_ina219_a(&mut self) -> Result<f64> {
+        self.ina219.read_a(&mut self.i2c)
+    }
+}
+
+fn record_measurements(i2c: &mut I2cDevices) -> Result<()> {
     let uptime_ms = uptime_usec() / 1000;
-    let rtc_ts = read_rtc_str(i2c)?;
-    let mv = get_oversampled_mv(adc_read_fn)?;
-    let mv = get_smoothed_mv(mv);
-    let mv = convert_adc_to_mv(mv);
-    let line = format!("{uptime_ms},{rtc_ts},{mv}");
+    let rtc_ts = i2c.read_ds3231_rtc_str()?;
+    let w = get_smoothed::<0>(i2c.read_ina219_w()?);
+    let v = get_smoothed::<1>(i2c.read_ina219_v()?);
+    let a = get_smoothed::<2>(i2c.read_ina219_a()?);
+    let line = format!("{uptime_ms},{rtc_ts},{w:.2},{v:.2},{a:.3}");
     log::info!("{line}");
-    append_data(&line)?;
+    // append_data(&line)?;
     Ok(())
 }
 
-fn main() -> Result<()> {
-    const SLEEP_USEC: u32 = 5 * 1000 * 1000;
+fn main() {
+    if let Err(e) = sub_main() {
+        log::info!("main error: {e}")
+    }
+    unsafe { esp_idf_svc::sys::esp_restart() }
+}
+fn sub_main() -> Result<()> {
+    const SLEEP_USEC: u32 = 2 * 1000 * 1000;
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
     log::set_max_level(log::LevelFilter::Debug);
     mount_storage()?;
     let peripherals = Peripherals::take()?;
-    let mut led = PinDriver::output(peripherals.pins.gpio8)?;
-    let adc_driver = AdcDriver::new(peripherals.adc1)?;
-    let mut adc = AdcChannelDriver::new(
-        &adc_driver,
-        peripherals.pins.gpio0,
-        &AdcChannelConfig {
-            attenuation: Atten11dB,
-            calibration: CalCurve,
-            resolution: Resolution12Bit,
-        },
-    )?;
-    let mut adc_read_fn = move || {
-        let raw = adc.read()?;
-        adc.raw_to_mv(raw)
-    };
-    let mut i2c = I2cDriver::new(
-        peripherals.i2c0,
-        peripherals.pins.gpio4,
-        peripherals.pins.gpio3,
-        &I2cConfig::new().baudrate(100.kHz().into()),
+    let mut i2c = I2cDevices::new(
+        I2cDriver::new(
+            peripherals.i2c0,
+            peripherals.pins.gpio4,
+            peripherals.pins.gpio3,
+            &I2cConfig::new().baudrate(400.kHz().into()), //TODO 100?
+        )?,
+        DS3231::new(0x68),
+        Ina219::new(0x41, 0.1, 3.2, 0x3FFF), // 0x3FFF based on https://www.ti.com/lit/ds/symlink/ina219.pdf
     )?;
 
+    //check if min voltage; maybe closure to record_measurements?
+
     if woke_from_sleep() {
-        record_voltage(&mut adc_read_fn, &mut i2c)?;
+        record_measurements(&mut i2c)?;
         reset_then_sleep(SLEEP_USEC.into());
     }
 
+    let mut led = PinDriver::output(peripherals.pins.gpio8)?;
     let sys_loop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
     let mut wifi = EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?;
@@ -280,16 +422,24 @@ fn main() -> Result<()> {
     wifi.set_configuration(&conf)?;
     wifi.start()?;
 
-    thread::scope(|s| -> Result<()> {
-        s.spawn(move || -> Result<()> {
-            loop {
+    thread::scope(|s| {
+        let r = Thread::new().stack_size(8000).spawn_scoped(s, move || {
+            let mut b = move || -> Result<()> {
                 led.set_low()?;
-                record_voltage(&mut adc_read_fn, &mut i2c)?;
+                record_measurements(&mut i2c)?;
                 led.set_high()?;
+                Ok(())
+            };
+            loop {
+                if let Err(e) = b() {
+                    log::info!("thread body error: {e}");
+                }
                 FreeRtos::delay_ms(SLEEP_USEC / 1000); // switch to use std::thread::sleep;use std::time::Duration;sleep(Duration::from_micros(SLEEP_USEC)); at end
             }
         });
-        Ok(())
-    })?;
+        if let Err(e) = r {
+            log::info!("thread error: {e}");
+        }
+    });
     Ok(())
 }
